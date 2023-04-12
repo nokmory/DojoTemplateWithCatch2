d0d27bcdb13a51fde1f1aa6c6496adde66d8dcb9
Michał Orynicz <michal.orynicz@nokia.com>
2023-04-12 17:51:44 +0200

    Add examples of simple test and a suite of tests

--- src/sampleTests.cpp
+++ src/sampleTests.cpp
@@ -1,1 +1,38 @@
-#include <iostream>
+#include "catch2/catch_all.hpp"
+
+namespace simple_test
+{
+TEST_CASE("very_simple_test")
+{
+  REQUIRE(true == true);
+}
+}
+
+namespace test_suite
+{
+TEST_CASE("test_suite_or_fixture")
+{
+    std::vector<int> testedEntity{};
+
+    SECTION("vector_is_initially_empty")
+    {
+        REQUIRE(testedEntity.empty());
+    }
+
+    SECTION("vector_after_putting_something_inside_has_size_1")
+    {
+        constexpr int something{1};
+        testedEntity.push_back(something);
+
+        REQUIRE(testedEntity.size() == 1);
+    }
+
+    SECTION("vector_after_putting_something_it_can_be_retrieved")
+    {
+        constexpr int somethingElse{5};
+        testedEntity.push_back(somethingElse);
+
+        REQUIRE(testedEntity.front() == somethingElse);
+    }
+}
+}
